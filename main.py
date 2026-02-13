1106bc48ea3d977824f373e41aba389cb80ce7fd
AndreyKosov <andrey.kosov.05@inbox.ru>
2026-02-13 17:30:03 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import logging
+
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer("deepvk/USER-bge-m3")
+
+app = FastAPI()
+
+
+class EmbeddingRequest(BaseModel):
+    text: str = Field(..., max_length=3000)
+
+
+@app.post("/embeddings")
+def get_embeddings(request: EmbeddingRequest):
+    embeddings = model.encode(request.text, normalize_embeddings=False)
+    return {"embeddings": embeddings.tolist()}
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
